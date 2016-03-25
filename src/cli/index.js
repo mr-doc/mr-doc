ee4d583d4c78e31eebc8b87e291c472724844b53
iwatakeshi <iwatakeshi@gmail.com>
2016-03-25 13:28:18 -0600

    :rocket:
    Improved CLI class.
    Created an Extension class.
    Updated package.

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,18 +1,20 @@
 /* eslint-env node */
 'use strict';
 
+const Extension = require('../utils/extension');
+const File = require('fs-extra');
 const Log = require('../utils/log');
-// const Path = require('path');
+const Liftoff = require('liftoff');
+const Option = require('mr-doc-utils').Option;
+const Path = require('path');
+const Promise = require('bluebird');
+const Yargs = require('yargs');
+const gstream = require('glob-stream');
+const isGlob = require('is-glob');
+const log = Log.global();
 const pkg = require('../../package.json');
 const rc = require('rc');
-const Option = require('mr-doc-utils').Option;
-const Yargs = require('yargs');
-const isGlob = require('is-glob');
 const _ = require('lodash');
-const Path = require('path');
-const Liftoff = require('liftoff');
-const gstream = require('glob-stream');
-const log = Log.global();
 
 class CLI {
   static parse() {
@@ -24,18 +26,24 @@ class CLI {
     .alias('help', 'h')
     .argv;
   }
-  static launch(argv) {
+  static get rocket() {
     // Create the CLI.
-    const rocket = new Liftoff({
+    return new Liftoff({
       name: 'mrdoc',
       processTitle: 'mrdoc',
       v8flags: require('v8flags'),
     });
-    // Launch it!
-    rocket.launch({
-      cwd: argv.cwd,
-      configPath: argv.mrdocrc,
-    }, env => CLI.handler(env, argv));
+  }
+  static launch(argv) {
+    return new Promise((resolve, reject) => {
+      // Launch the CLI!
+      CLI.rocket.launch({
+        cwd: argv.cwd,
+        configPath: argv.mrdocrc,
+      }, env => CLI.handler(env, argv)
+      .then(stream => resolve(stream))
+      .catch(error => reject(error)));
+    });
   }
   static handler(env, options) {
     const version = options.version || options.v;
@@ -48,20 +56,45 @@ class CLI {
       log.warn(`${log.color.yellow('No source specified!')} See --help for usage.`);
       process.exit();
     }
-    // Get the options.
-    const opts = rc('mrdoc', Option.merge(options));
-    // Split the string if it contains commas.
-    let sources = opts.mrdoc.source.indexOf(',') > -1 ?
-    opts.mrdoc.source.split(',') : opts.mrdoc.source;
-    // Check if sources is not an array.
-    if (_.isString(sources) && !isGlob(sources)) {
-      // Make sure the path is resolved.
-      sources = Path.resolve(opts.mrdoc.cwd, sources);
-      // Check to see if '/' is at the end.
-      sources = sources[sources.length - 1] === '/' ?
-      sources : `${sources}/`;
-    }
-    return gstream.create(sources, { cwd: opts.mrdoc.cwd });
+    return new Promise((resolve, reject) => {
+      // Get the options.
+      const opts = rc('mrdoc', Option.merge(options));
+      const sources = opts.mrdoc.source.split(',')
+      .map(path => path.trim())
+      .map(path => {
+        // Check if the path is not in glob pattern.
+        if (!isGlob(path)) {
+          // Make sure the path is resolved.
+          let str = Path.resolve(opts.mrdoc.cwd, path);
+          // Check if the path is a file or directory.
+          if (_.isEmpty(Path.parse(path).ext)) {
+            // Check if the path has a '/' at the end.
+            str = str[str.length - 1] === '/' ?
+            str : `${str}/`;
+          }
+          // Make sure the file or directory exists;
+          if (File.existsSync(str)) {
+            // Check if the directory has sub-directories.
+            const hasSubDirs = File.readdirSync(str)
+            .filter(file =>
+              File.statSync(Path.join(str, file)).isDirectory()).length > 1;
+            // Get the file extension.
+            const extension = Extension.find(opts.parser.language);
+            // Set the glob pattern based on 'hasSubDirs'.
+            str = hasSubDirs ?
+            `${str}**${Path.sep}${extension}` : `${str}*${extension}`;
+          } else return null;
+          return str;
+        }
+        return path;
+      });
+      // DEBUG: Sources
+      log.debug(log.color.blue('Sources:'), sources);
+      if (sources.indexOf(null) > -1) {
+        reject({ level: 'warn', message: log.color.yellow(
+          `${_.isArray(sources) ? sources.join(', ') : sources} does not exist!`) });
+      } else resolve(gstream.create(sources, { cwd: opts.mrdoc.cwd }));
+    });
   }
 }
 
--- src/utils/extension.js
+++ src/utils/extension.js
@@ -1,5 +1,17 @@
-module.exports = {
-  javascript: '.js',
-  json: '.json',
-  typescript: '.ts',
-};
+'use strict';
+
+class Extension {
+  static find(language) {
+    return Extension.extensions()[language];
+  }
+  static extensions() {
+    return {
+      javascript: '.js',
+      json: '.json',
+      html: '.html',
+      typescript: '.ts',
+    };
+  }
+}
+
+module.exports = Extension;
